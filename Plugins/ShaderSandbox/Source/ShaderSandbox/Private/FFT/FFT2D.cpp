24c554be784a654d3d59d7812db375935ba73708
monguri <mongry@gmail.com>
2020-02-22 18:44:27 +0900

    まずはフォワードFFTの横方向のFFTだけ実装したので一旦保存のためにコミット

--- Plugins/ShaderSandbox/Source/ShaderSandbox/Private/FFT/FFT2D.cpp
+++ Plugins/ShaderSandbox/Source/ShaderSandbox/Private/FFT/FFT2D.cpp
@@ -4,17 +4,20 @@
 #include "RenderGraphBuilder.h"
 #include "RenderGraphUtils.h"
 #include "RHIStaticStates.h"
+#include "RenderTargetPool.h"
 
 // PostProcessFFTBloom.cppÇéQçlÇ…ÇµÇƒÇ¢ÇÈ
 
 namespace FFT2D
 {
-class FForwardFFT2D512x512CS : public FGlobalShader
+class FTwoForOneRealFFTImage1D512x512 : public FGlobalShader
 {
-	DECLARE_GLOBAL_SHADER(FForwardFFT2D512x512CS);
-	SHADER_USE_PARAMETER_STRUCT(FForwardFFT2D512x512CS, FGlobalShader);
+	DECLARE_GLOBAL_SHADER(FTwoForOneRealFFTImage1D512x512);
+	SHADER_USE_PARAMETER_STRUCT(FTwoForOneRealFFTImage1D512x512, FGlobalShader);
 
 	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+		SHADER_PARAMETER(FIntRect, SrcRect)
+		SHADER_PARAMETER(FIntRect, DstRect)
 		SHADER_PARAMETER_TEXTURE(Texture2D<float4>, SrcTexture)
 		SHADER_PARAMETER_SAMPLER(SamplerState, SrcSampler)
 		SHADER_PARAMETER_UAV(RWTexture2D<float4>, DstTexture)
@@ -27,28 +30,50 @@ public:
 	}
 };
 
-IMPLEMENT_GLOBAL_SHADER(FForwardFFT2D512x512CS, "/Plugin/ShaderSandbox/Private/FFT.usf", "ForwardFFT2D512x512CS", SF_Compute);
+IMPLEMENT_GLOBAL_SHADER(FTwoForOneRealFFTImage1D512x512, "/Plugin/ShaderSandbox/Private/FFT.usf", "TwoForOneRealFFTImage1D512x512", SF_Compute);
 
 void DoFFT2D512x512(FRHICommandListImmediate& RHICmdList, EFFTMode Mode, const FTextureRHIRef& SrcTexture, FRHIUnorderedAccessView* DstUAV)
 {
+	const FIntRect& SrcRect = FIntRect(FIntPoint(0, 0), FIntPoint(512, 512));
+	const uint32 FREQUENCY_PADDING = 2; //TODO:óùã¸ÇóùâÇ≈Ç´ÇƒÇ»Ç¢
+	const FIntPoint& TmpBufferSize = FIntPoint(512 + FREQUENCY_PADDING, 512);
+	const FIntRect& TmpRect = FIntRect(FIntPoint(0, 0), TmpBufferSize);
+
 	FRDGBuilder GraphBuilder(RHICmdList);
 
 	TShaderMap<FGlobalShaderType>* ShaderMap = GetGlobalShaderMap(ERHIFeatureLevel::SM5);
 
-	TShaderMapRef<FForwardFFT2D512x512CS> ForwardFFTCS(ShaderMap);
+	{
+		TShaderMapRef<FTwoForOneRealFFTImage1D512x512> TwoForOneCS(ShaderMap);
 
-	FForwardFFT2D512x512CS::FParameters* ForwardFFTParams = GraphBuilder.AllocParameters<FForwardFFT2D512x512CS::FParameters>();
-	ForwardFFTParams->SrcTexture = SrcTexture;
-	ForwardFFTParams->SrcSampler = TStaticSamplerState<SF_Bilinear, AM_Wrap, AM_Wrap, AM_Wrap>::GetRHI();
-	ForwardFFTParams->DstTexture = DstUAV;
+		FPooledRenderTargetDesc Desc = FPooledRenderTargetDesc::Create2DDesc(
+			TmpBufferSize,
+			EPixelFormat::PF_A32B32G32R32F,
+			FClearValueBinding::None, 
+			TexCreate_None,
+			TexCreate_RenderTargetable | TexCreate_UAV,
+			false
+		);
 
-	FComputeShaderUtils::AddPass(
-		GraphBuilder,
-		RDG_EVENT_NAME("ForwardFFT2D512x512CS"),
-		*ForwardFFTCS,
-		ForwardFFTParams,
-		FIntVector(512, 1, 1)
-	);
+		// TODO:Ç±ÇÍÇÕà»ëOé¿å±ÇµÇΩRDGTextureÇÃèëÇ´ï˚Ç≈èëÇØÇÈÇ©Ç‡
+		TRefCountPtr<IPooledRenderTarget> TmpRenderTarget;
+		GRenderTargetPool.FindFreeElement(RHICmdList, Desc, TmpRenderTarget, TEXT("FFT2D Tmp Buffer"));
+
+		FTwoForOneRealFFTImage1D512x512::FParameters* ForwardFFTParams = GraphBuilder.AllocParameters<FTwoForOneRealFFTImage1D512x512::FParameters>();
+		ForwardFFTParams->SrcTexture = SrcTexture;
+		ForwardFFTParams->SrcSampler = TStaticSamplerState<SF_Bilinear, AM_Wrap, AM_Wrap, AM_Wrap>::GetRHI();
+		ForwardFFTParams->DstTexture = TmpRenderTarget->GetRenderTargetItem().UAV;
+		ForwardFFTParams->SrcRect = SrcRect;
+		ForwardFFTParams->DstRect = TmpRect;
+
+		FComputeShaderUtils::AddPass(
+			GraphBuilder,
+			RDG_EVENT_NAME("TwoForOneRealFFTImage1D512x512"),
+			*TwoForOneCS,
+			ForwardFFTParams,
+			FIntVector(512, 1, 1)
+		);
+	}
 
 	GraphBuilder.Execute();
 }
