70f15f280da9b120191c2113e1e3f9f51c41ae7b
monguri <mongry@gmail.com>
2020-02-23 17:36:29 +0900

    InverseFFTの横方向を実装した

--- Plugins/ShaderSandbox/Source/ShaderSandbox/Private/FFT/FFT2D.cpp
+++ Plugins/ShaderSandbox/Source/ShaderSandbox/Private/FFT/FFT2D.cpp
@@ -16,6 +16,7 @@ class FTwoForOneRealFFTImage1D512x512 : public FGlobalShader
 	SHADER_USE_PARAMETER_STRUCT(FTwoForOneRealFFTImage1D512x512, FGlobalShader);
 
 	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+		SHADER_PARAMETER(uint32, Forward)
 		SHADER_PARAMETER(FIntPoint, SrcRectMin)
 		SHADER_PARAMETER(FIntPoint, SrcRectMax)
 		SHADER_PARAMETER(FIntRect, DstRect)
@@ -39,6 +40,7 @@ class FComplexFFTImage1D512x512 : public FGlobalShader
 	SHADER_USE_PARAMETER_STRUCT(FComplexFFTImage1D512x512, FGlobalShader);
 
 	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+		SHADER_PARAMETER(uint32, Forward)
 		SHADER_PARAMETER(FIntPoint, SrcRectMin)
 		SHADER_PARAMETER(FIntPoint, SrcRectMax)
 		SHADER_PARAMETER(FIntRect, DstRect)
@@ -64,6 +66,9 @@ void DoFFT2D512x512(FRHICommandListImmediate& RHICmdList, EFFTMode Mode, const F
 	const FIntRect& TmpRect = FIntRect(FIntPoint(0, 0), TmpBufferSize);
 	const FIntRect& DstRect = SrcRect;
 
+	const FIntPoint& TmpBufferSize2 = FIntPoint(512, 512);
+	const FIntRect& TmpRect2 = SrcRect;
+
 	FRDGBuilder GraphBuilder(RHICmdList);
 
 	TShaderMap<FGlobalShaderType>* ShaderMap = GetGlobalShaderMap(ERHIFeatureLevel::SM5);
@@ -85,6 +90,7 @@ void DoFFT2D512x512(FRHICommandListImmediate& RHICmdList, EFFTMode Mode, const F
 		GRenderTargetPool.FindFreeElement(RHICmdList, Desc, TmpRenderTarget, TEXT("FFT2D Tmp Buffer"));
 
 		FTwoForOneRealFFTImage1D512x512::FParameters* TwoForOneForwardFFTParams = GraphBuilder.AllocParameters<FTwoForOneRealFFTImage1D512x512::FParameters>();
+		TwoForOneForwardFFTParams->Forward = 1;
 		TwoForOneForwardFFTParams->SrcTexture = SrcTexture;
 		TwoForOneForwardFFTParams->SrcSampler = TStaticSamplerState<SF_Bilinear, AM_Wrap, AM_Wrap, AM_Wrap>::GetRHI();
 		TwoForOneForwardFFTParams->DstTexture = TmpRenderTarget->GetRenderTargetItem().UAV;
@@ -95,27 +101,51 @@ void DoFFT2D512x512(FRHICommandListImmediate& RHICmdList, EFFTMode Mode, const F
 
 		FComputeShaderUtils::AddPass(
 			GraphBuilder,
-			RDG_EVENT_NAME("TwoForOneRealFFTImage1D512x512"),
+			RDG_EVENT_NAME("TwoForOneRealFFTImage1D512x512ForwardHorizontal"),
 			*TwoForOneForwardFFTCS,
 			TwoForOneForwardFFTParams,
 			FIntVector(512, 1, 1)
 		);
 
+		TRefCountPtr<IPooledRenderTarget> TmpRenderTarget2;
+		GRenderTargetPool.FindFreeElement(RHICmdList, Desc, TmpRenderTarget2, TEXT("FFT2D Tmp Buffer2"));
+
 		TShaderMapRef<FComplexFFTImage1D512x512> ComplexForwardFFTCS(ShaderMap);
 
 		FComplexFFTImage1D512x512::FParameters* ComplexForwardFFTParams = GraphBuilder.AllocParameters<FComplexFFTImage1D512x512::FParameters>();
+		ComplexForwardFFTParams->Forward = 1;
 		ComplexForwardFFTParams->SrcTexture = TmpRenderTarget->GetRenderTargetItem().ShaderResourceTexture;
 		ComplexForwardFFTParams->SrcSampler = TStaticSamplerState<SF_Bilinear, AM_Wrap, AM_Wrap, AM_Wrap>::GetRHI();
-		ComplexForwardFFTParams->DstTexture = DstUAV;
+		ComplexForwardFFTParams->DstTexture = TmpRenderTarget2->GetRenderTargetItem().UAV;
 		ComplexForwardFFTParams->SrcRectMin = TmpRect.Min;
 		ComplexForwardFFTParams->SrcRectMax = TmpRect.Max;
-		ComplexForwardFFTParams->DstRect = DstRect;
+		ComplexForwardFFTParams->DstRect = TmpRect2;
 
 		FComputeShaderUtils::AddPass(
 			GraphBuilder,
-			RDG_EVENT_NAME("ComplexFFTImage1D512x512"),
+			RDG_EVENT_NAME("ComplexFFTImage1D512x512ForwardVertical"),
 			*ComplexForwardFFTCS,
 			ComplexForwardFFTParams,
+			FIntVector(512 + FREQUENCY_PADDING, 1, 1)
+		);
+
+
+		TShaderMapRef<FComplexFFTImage1D512x512> ComplexInverseFFTCS(ShaderMap);
+
+		FComplexFFTImage1D512x512::FParameters* ComplexInverseFFTParams = GraphBuilder.AllocParameters<FComplexFFTImage1D512x512::FParameters>();
+		ComplexInverseFFTParams->Forward = 0;
+		ComplexInverseFFTParams->SrcTexture = TmpRenderTarget2->GetRenderTargetItem().ShaderResourceTexture;
+		ComplexInverseFFTParams->SrcSampler = TStaticSamplerState<SF_Bilinear, AM_Wrap, AM_Wrap, AM_Wrap>::GetRHI();
+		ComplexInverseFFTParams->DstTexture = TmpRenderTarget->GetRenderTargetItem().UAV;
+		ComplexInverseFFTParams->SrcRectMin = TmpRect2.Min;
+		ComplexInverseFFTParams->SrcRectMax = TmpRect2.Max;
+		ComplexInverseFFTParams->DstRect = TmpRect;
+
+		FComputeShaderUtils::AddPass(
+			GraphBuilder,
+			RDG_EVENT_NAME("ComplexFFTImage1D512x512InverseHorizontal"),
+			*ComplexInverseFFTCS,
+			ComplexInverseFFTParams,
 			FIntVector(512 + FREQUENCY_PADDING, 1, 1)
 		);
 	}
