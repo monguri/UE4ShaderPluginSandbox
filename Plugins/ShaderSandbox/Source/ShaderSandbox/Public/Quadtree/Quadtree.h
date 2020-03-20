a951656affabfd4b4adff3e0fe9bae8afae2e55a
monguri <mongry@gmail.com>
2020-03-20 20:16:08 +0900

    FQuadNodeの各メンバ変数に初期値を設定した

--- Plugins/ShaderSandbox/Source/ShaderSandbox/Public/Quadtree/Quadtree.h
+++ Plugins/ShaderSandbox/Source/ShaderSandbox/Public/Quadtree/Quadtree.h
@@ -8,13 +8,13 @@ namespace Quadtree
 struct FQuadNode
 {
 	/** Position of bottom left corner. */
-	FVector2D BottomLeft;
+	FVector2D BottomLeft = FVector2D::ZeroVector;
 	/** Length of edge. */
-	float Length;
+	float Length = 0.0f;
 	/** LOD level. */
-	int32 Lod;
+	int32 LOD = 0;
 	/** Indices of child nodes in render list. If a child does not exist, it is INDEX_NONE. */
-	int32 ChildNodeIndices[4];
+	int32 ChildNodeIndices[4] = {INDEX_NONE, INDEX_NONE, INDEX_NONE, INDEX_NONE};
 };
 
 /** 
