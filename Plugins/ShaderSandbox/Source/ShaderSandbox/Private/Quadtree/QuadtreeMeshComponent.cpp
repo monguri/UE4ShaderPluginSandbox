fdb53d06665b146c22f71a6d6570354c388f1f8b
monguri <mongry@gmail.com>
2020-03-24 16:54:49 +0900

    Revert "カメラの真下の部分から最も近いグリッドを面積評価に選んでいると、カメラが近いメッシュと遠いメッシュで評価値に差ができすぎるので、"
    This reverts commit 7bce581fcc7ccc66da24a3348d770e986d886cc6.

--- Plugins/ShaderSandbox/Source/ShaderSandbox/Private/Quadtree/QuadtreeMeshComponent.cpp
+++ Plugins/ShaderSandbox/Source/ShaderSandbox/Private/Quadtree/QuadtreeMeshComponent.cpp
@@ -121,14 +121,7 @@ public:
 				TArray<FQuadNode> RenderList;
 				RenderList.Reserve(512); //TODO:とりあえず512。ここで毎回確保は処理不可が無駄だが、この関数がconstなのでとりあえず
 
-				const FVector& CameraLocation = View->ViewMatrices.GetViewOrigin();
-				float QuadtreePlaneZ = GetLocalToWorld().GetOrigin().Z;
-				float CameraHeightFromPlane = CameraLocation.Z - QuadtreePlaneZ;
-				const FVector& CameraRayDir = View->ViewMatrices.GetInvViewMatrix().TransformVector(FVector::UpVector);
-				float CameraRayLength = CameraHeightFromPlane / FMath::Abs(CameraRayDir.Z);
-				const FVector& CameraRayIntersectionPoint = CameraLocation + CameraRayDir * CameraRayLength;
-
-				Quadtree::BuildQuadtree(MaxLOD, NumGridRowColumn, MaxScreenCoverage, PatchLength, CameraRayIntersectionPoint, View->ViewMatrices.GetViewProjectionMatrix(), RootNode, QuadNodeList, RenderList);
+				Quadtree::BuildQuadtree(MaxLOD, NumGridRowColumn, MaxScreenCoverage, PatchLength, View->ViewMatrices.GetViewOrigin(), View->ViewMatrices.GetViewProjectionMatrix(), RootNode, QuadNodeList, RenderList);
 
 				for (const FQuadNode& Node : RenderList)
 				{
